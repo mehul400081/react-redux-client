744b6c25b58c8888701bbf680b73960b9db05307
mehul400081 <mehul.bhatt2004@gmail.com>
2018-02-17 19:54:37 +0100

    added validation condition

--- src/components/person-add.js
+++ src/components/person-add.js
@@ -10,7 +10,13 @@ class PersonAdd extends Component {
         var user = findDOMNode(this.refs.name).value.trim() 
         var email = findDOMNode(this.refs.email).value.trim()
         var age = findDOMNode(this.refs.age).value.trim()
-        this.props.onAdd({ "name": user, "email": email, "age": age });
+        
+        if(user.length > 0 && email.length > 0 && age.length > 0 ){
+
+            this.props.onAdd({ "name": user, "email": email, "age": age });
+        }else{
+            findDOMNode(this.refs.validationLabel).value = 'please enter mandatory field';
+        }
     }
     render() {
         return (
@@ -20,16 +26,18 @@ class PersonAdd extends Component {
                         <div className="text-center">
                             <h3> Add Person </h3>
                         </div>
+                       
+                        <br></br>
                         <FormGroup controlId="formControlsUser" type="text" >
-                            <ControlLabel>Name</ControlLabel>
+                            <ControlLabel>Name*</ControlLabel>
                             <FormControl ref="name" placeholder="Name" />
                         </FormGroup>
                         <FormGroup controlId="formControlsEmail" type="text" >
-                            <ControlLabel>Email</ControlLabel>
+                            <ControlLabel>Email*</ControlLabel>
                             <FormControl ref="email" placeholder="Email" />
                         </FormGroup>
                         <FormGroup controlId="formControlsAge" type="text" >
-                            <ControlLabel>Age</ControlLabel>
+                            <ControlLabel>Age*</ControlLabel>
                             <FormControl ref="age" placeholder="Age" />
                         </FormGroup>
                         <Button block bsStyle="warning" type="submit" onClick={this.handlePersonAdd.bind(this)}>Add</Button>
